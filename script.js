bb4655dd6581401be0ed73b96b5bb4ff74c7a836
Nazir Khashiev <nxashiev@gmail.com>
2022-06-07 14:47:20 +0300

    added class

--- script.js
+++ script.js
@@ -31,3 +31,59 @@ button.addEventListener("click", () => {
     input1.value = "";
   }
 });
+
+// class Calc {
+//   constructor() {
+//     this.input1 = document.querySelector(".input__write");
+//     this.input2 = document.querySelector(".input__read");
+//     this.button = document.querySelector(".btn");
+//     this.keyword = this.input1.value.split(" ")[0];
+
+//     this.button.addEventListener("click", this.func);
+//   }
+
+//   func() {
+//     switch (this.keyword) {
+//       case "var":
+//         this.var();
+//         break;
+//       case "let":
+//         this.let();
+//         break;
+//       case "fn":
+//         this.fn();
+//         break;
+//       case "print":
+//         this.print();
+//         break;
+//       default:
+//         console.log("ошибка");
+//     }
+//   }
+
+//   var() {
+//     this.input2.value += `${this.input1.value}\n`;
+//     this.input1.value = "";
+//     console.log("объявление");
+//   }
+
+//   let() {
+//     this.input2.value += `${this.input1.value}\n`;
+//     this.input1.value = "";
+//     console.log("присваивание");
+//   }
+
+//   fn() {
+//     this.input2.value += `${this.input1.value}\n`;
+//     this.input1.value = "";
+//     console.log("подсчет");
+//   }
+
+//   print() {
+//     this.input2.value += `${this.input1.value}\n`;
+//     this.input1.value = "";
+//     console.log("вывод");
+//   }
+// }
+
+// let calc = new Calc();
